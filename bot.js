5c5942dd9d268441bed6d0bb0f1e145afc64446b
Mike Constanza <michael.constanza@gmail.com>
2017-01-23 01:03:36 -0500

    added env vars for heroku

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 // REDDIT LIBRARY AND SETUP ////////////////////////////////////////
 
 // this is our private config file with relevant application information for both Reddit and nodemailer
-var redditConfig = require('./config.js');
+if(NODE_ENV=development){
+  var redditConfig = require('./config.js');
+}
 
 // This is a wrapper for the Reddit api. It isn't necessary, but it makes working with the API easier
 var snoowrap = require('snoowrap');
@@ -9,10 +11,10 @@ var snoowrap = require('snoowrap');
 // configure the reddit library using all of your super-secret config data
 const reddit = new snoowrap({
     userAgent: 'Javascript bot that compiles web-dev articles and posts them in /r/RCBRedditBot',
-    clientId: redditConfig.clientId,
-    clientSecret: redditConfig.clientSecret,
-    username: redditConfig.username,
-    password: redditConfig.password});
+    clientId: redditConfig.clientId || REDDIT_CLIENT,
+    clientSecret: redditConfig.clientSecret || REDDIT_SECRET,
+    username: redditConfig.username || REDDIT_USER,
+    password: redditConfig.password|| REDDIT_PASSWORD});
 
 // Variables used in application
 
